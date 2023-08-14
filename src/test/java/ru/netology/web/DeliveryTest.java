b02671a5dc696b6e149a456c77691c39e0a68a79
Ramzesito <roman_shv@mail.ru>
2023-08-14 11:42:51 +0300

    apdated tests and added .appveyor.yml

--- src/test/java/ru/netology/web/DeliveryTest.java
+++ src/test/java/ru/netology/web/DeliveryTest.java
@@ -1,12 +1,21 @@
 package ru.netology.web;
 
-import org.junit.jupiter.api.Assertions;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class DeliveryTest {
+
+    //Faker faker = new Faker();
 
     @BeforeEach
     void setup() {
@@ -16,13 +25,34 @@ public class DeliveryTest {
     @Test
     @DisplayName("Should successful plan and replan meeting")
     public void shouldSuccessfulPlanAndReplanMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
 
+        $(By.cssSelector("[data-test-id='city'] input")).setValue(validUser.getCity());
+        $(By.cssSelector("[data-test-id='date'] input")).sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE); // очистка поля ввода
+        $(By.cssSelector("[data-test-id='date'] input")).sendKeys(firstMeetingDate);
+        $(By.cssSelector("[data-test-id='name'] input")).setValue(validUser.getName());
+        $(By.cssSelector("[data-test-id='phone'] input")).setValue(validUser.getPhone());
+        $(By.cssSelector("[data-test-id='agreement']")).click();
+        $(byText("Запланировать")).click();
+        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='success-notification'] .notification__content").
+                shouldBe(Condition.visible).
+                shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate));
 
-
-        //Assertions.assertTrue(true);
+        $(By.cssSelector("[data-test-id='date'] input")).sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE); // очистка поля ввода
+        $(By.cssSelector("[data-test-id='date'] input")).sendKeys(secondMeetingDate);
+        $(byText("Запланировать")).click();
+        $("[data-test-id='replan-notification'] .notification__content").
+                shouldBe(Condition.visible).
+                shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+        $(By.cssSelector("[data-test-id='replan-notification'] button")).click();
+        $("[data-test-id='success-notification'] .notification__content").
+                shouldBe(Condition.visible).
+                shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate));
     }
-
-
-
 
 }
